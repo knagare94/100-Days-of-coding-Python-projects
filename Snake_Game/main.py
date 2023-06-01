3f2dd3aa841e0c3db055c6daf7bd1641ebd0bfa0
Kalpesh Nagare <64601002+knagare94@users.noreply.github.com>
2023-06-01 18:05:16 +0530

    Add files via upload

--- Snake_Game/main.py
+++ Snake_Game/main.py
@@ -34,14 +34,14 @@ while is_game_on:
 
     # detect collision with wall
     if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
-        is_game_on = False
-        scoreboard.game_over()
+        scoreboard.reset()
+        snake.reset()
 
     # detect collision with tail
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            is_game_on = False
-            scoreboard.game_over()
+            scoreboard.reset()
+            snake.reset()
 
 
 screen.exitonclick()
